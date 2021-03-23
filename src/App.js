2f464d1416bdbc4c0eaa53dc81f7f403ac4d0536
David De La Rosa <davidrr261096@hotmail.com>
2021-03-23 15:32:13 -0400

    creating a sidebar to implement the element remove feature

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React, { useEffect, useState } from 'react'
-import "grapesjs/dist/css/grapes.min.css"
-import Grapesjs from "grapesjs"
-import gjsPresetWebpage from "grapesjs-preset-webpage"
-import "./App.css"
+import React, { useEffect, useState } from 'react';
+import Grapesjs from 'grapesjs';
+import gjsPresetWebpage from 'grapesjs-preset-webpage';
+import { FormGroup, FormControlLabel, Switch } from '@material-ui/core';
+import 'grapesjs/dist/css/grapes.min.css';
+import './App.css';
 
 function App() {
-  const [editor, setEditor] = useState(null)
+  const [editor, setEditor] = useState(null);
+  const [subtitleChecked, setSubtitleChecked] = useState(true);
+  const [titleChecked, setTitleChecked] = useState(true);
 
   useEffect(() => {
     if (!editor) {
       const e = Grapesjs.init({
-          container: `#gjs-container`,
-          fromElement: true,
-          plugins: [gjsPresetWebpage],
-      })
-      setEditor(e)
-  }
-  }, [editor])
-  
+        container: `#gjs-container`,
+        fromElement: true,
+        plugins: [gjsPresetWebpage],
+      });
+      setEditor(e);
+    }
+  }, [editor]);
+
+  const toggleChecked = (e, setChecked) => {
+    const checked = e.target.checked;
+    const getComponent = editor.getWrapper().find(`.lead-${e.target.name}`)[0];
+
+    checked
+      ? getComponent.set('style', { display: 'block' })
+      : getComponent.set('style', { display: 'none' });
+
+    setChecked(checked);
+  };
+
   return (
     <div>
-      <div id="gjs-container"/>
+      <aside className="sidebar">
+        <FormGroup className="sidebar-formgroup">
+          <FormControlLabel
+            control={
+              <Switch
+                name="title"
+                checked={titleChecked}
+                onChange={(e) => toggleChecked(e, setTitleChecked)}
+              />
+            }
+            label="Title"
+          />
+          <FormControlLabel
+            control={
+              <Switch
+                name="subtitle"
+                checked={subtitleChecked}
+                onChange={(e) => toggleChecked(e, setSubtitleChecked)}
+              />
+            }
+            label="Subtitle"
+          />
+        </FormGroup>
+      </aside>
+      <div id="gjs-container" />
     </div>
   );
 }
